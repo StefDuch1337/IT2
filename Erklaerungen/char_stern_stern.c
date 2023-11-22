d1a2c7daed21dae4bd2e52da9a20071eb1ac6bb8
StefDuch1337 <stefan.m.duchow@gmail.com>
2023-11-22 16:58:08 +0100

    22_11_2023

--- Erklaerungen/char_stern_stern.c
+++ Erklaerungen/char_stern_stern.c
@@ -32,3 +32,35 @@ int main() {
 
 // firstCharacter wird auf das erste Zeichen der ersten Zeichenkette ('A') gesetzt.
 // Hier verwenden wir *firstString, um den Wert zu erhalten, auf den firstString zeigt, was das erste Zeichen der Zeichenkette ist.
+
+
+//1. Ein Array von Zeichenketten:
+char* strings[] = {"Apple", "Banana", "Orange"};
+//2. Ein Zeiger auf den Anfang des Zeiger-Arrays:
+char** pointerArray = strings;
+//pointerArray ist ein Zeiger auf einen Zeiger auf Zeichen (char**).
+//Es wird auf den Anfang des Zeiger-Arrays strings gesetzt.
+//3. Dereferenzierung des Zeigers auf den Anfang des Zeiger-Arrays:
+char* firstString = *pointerArray;
+//firstString ist ein Zeiger auf Zeichen (char*).
+//Es wird auf das dereferenziert, worauf pointerArray zeigt, also auf die Adresse der ersten Zeichenkette "Apple".
+//4. Dereferenzierung eines Zeigers auf eine Zeichenkette (char):*
+char firstCharacter = *firstString;
+//firstCharacter ist ein einzelnes Zeichen (char).
+//Es wird auf das dereferenziert, worauf firstString zeigt, also auf das erste Zeichen der Zeichenkette "Apple".
+
+// Zusammengefasst:
+
+// strings ist ein Array von Zeigern auf Zeichenketten.
+// pointerArray ist ein Zeiger auf den Anfang dieses Zeiger-Arrays.
+// firstString ist ein Zeiger auf den Anfang der ersten Zeichenkette im Array.
+// firstCharacter ist das erste Zeichen dieser ersten Zeichenkette.
+// In diesem Beispiel sind die Datentypen wichtig, um sicherzustellen, dass die Dereferenzierungen korrekt erfolgen und die richtigen Werte erhalten werden.
+
+
+
+
+
+
+
+
